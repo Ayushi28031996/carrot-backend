c3640f06b5f4f46991ae1cac28419d280b9f1c04
Ayushi <ayushi.somani@indianic.com>
2023-06-26 13:23:01 +0530

    Commit

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# carrot-backend
+# IndiaNIC Carrot test
 
-npm start
+## Prequisites (Development)
+
+| Module | Version |
+| --- | --- |
+| Node | 16.16.0 |
+| Npm | 6.13.0 |
+| Postgres | 15 |
+
+
+##### Running project in local
+``` bash
+> npm install
+
+> npm start
+
+```
+------------
+
+######
+You need to convert .env.sample to .env and start run project
--- app.js
+++ app.js
@@ -8,77 +8,37 @@ const User = require('./app/modules/User/Schema.js')
 const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql');
 const { makeExecutableSchema } = require('@graphql-tools/schema')
+
+
+// DB FUnction Start 
 async function dbfunction() {
   await database.databaseConnection()
 }
+dbfunction()
+// DB Function End
+
+
+// User List Functions Start with pagination
 const getPagingData = (data, page, limit) => {
   const { count: totalItems, rows: usersList } = data;
   const currentPage = page ? +page : 0;
   const totalPages = Math.ceil(totalItems / limit);
   return { totalItems, usersList, totalPages, currentPage };
 };
-
-dbfunction()
-async function userFind() {
-  // const { page, size } = {1 , 10}
-  const page = 1
-  const size = 10
+async function userFind(page,size) {
   const offset = (page - 1) * size;
   const limit = size;
-  let count = await User.userSchema.findAndCountAll({ limit, offset, raw: true })
+  let count = await User.userSchema.findAndCountAll({ limit, offset })
   const response = getPagingData(count, page, limit);
-  return response.usersList;
+  return response;
 }
-const schema = buildSchema(`
-  type User {
-    id: ID!
-    first_name: String!
-    last_name: String!
-    gender: String!
-  }
+// User List Functions End with pagination
 
-  type Query {
-    
-    users: [User!]!,
-   
-  }
-`);
 
-const root = {
-
-  users: async () => await userFind(),
-
-};
-
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    rootValue: root,
-    graphiql: true,
-  })
-);
-
-app.get('/userList', async (req, res) => {
-  try {
-    const { page, size } = req.query;
-    const offset = (page - 1) * size;
-    const limit = size;
-    let count = await User.userSchema.findAndCountAll({ limit, offset })
-    const response = getPagingData(count, page, limit);
-    res.send(response);
-  }
-  catch (e) {
-    console.log(e);
-    res.send(e)
-  }
-
-});
-
-app.get('/geolocation', async (req, res) => {
-  const lat = 28.62;
-  const long = 79.82;
-  const distance = 1;
+// Find users within specific geo radius start 
+async function geolocation(latitude,longitude){
+  const lat = latitude;
+  const long = longitude;
   const haversine = `(
         6371 * acos(
             cos(radians(${lat}))
@@ -103,12 +63,74 @@ app.get('/geolocation', async (req, res) => {
     let user = await User.userSchema.findOne({ where: { id: m.userId } })
     finalRes.push({ 'first_name': user.first_name, 'last_name': user.last_name, 'gender': user.gender, location: { lat: m.lat, long: m.long } })
   }
+  console.log(finalRes)
+  return finalRes;
+}
+// Find users within specific geo radius End 
 
-  res.send(finalRes)
+// Build Schema .gql Start
+const schema1 = buildSchema(`
+  type User {
+    id: ID!
+    first_name: String!
+    last_name: String!
+    gender: String!
+    
+  }
+
+  type Query {
+    totalItems : Int 
+    users: [User!]!
+    totalPages  : Int 
+    currentPage : Int 
+  }
+`);
+const schema = buildSchema(`
+type Location{
+  lat:Float!
+  long:Float!
+}
+type User {
+  first_name: String!
+  last_name: String!
+  gender: String!
+  location: Location
+}
+type Query {
+  findusers(radius: Int): [User!]!
+}
+`);
+// Build Schema .gql End
 
 
-})
+// Resolvers in Graphql Start
+const root = {
+  totalItems : async () => (await userFind(1,10)).totalItems,
+  users: async () => (await userFind(1,10)).usersList,
+  totalPages : async () => (await userFind(1,10)).totalPages,
+  currentPage : async () => (await userFind(1,10)).currentPage
+};
+const Resolvers = {
+  findusers: async(radius=1)=> ( geolocation(28.62,79.82))
+};
+// Resolvers in Graphql End
+
+
+// Graphql Start
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: Resolvers,
+    graphiql: true,
+  })
+);
+// Graphql End
+
+
+// App Listen At Server Start
 app.listen(process.env.PORT, () => {
   console.log(`Now listening on port ${process.env.PORT}`);
 });
+// App Listen At Server End
 
